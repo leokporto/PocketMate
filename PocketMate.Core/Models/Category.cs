b4a89855d0dffe98bc0d24439ac0744198157fdc
Leonardo Porto <leokporto@gmail.com>
2024-06-07 08:17:32 -0300

    Added solution and core project

---
+++ PocketMate.Core/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketMate.Core.Models
+{
+	public class Category
+	{
+		public long Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public string Description { get; set; } = string.Empty;
+
+		public string UserId { get; set; } = string.Empty;
+	}
+}
