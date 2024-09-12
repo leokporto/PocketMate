8b7e974adebb8c8be9728acf488ba72c3f128f5f
Leonardo Porto <leokporto@gmail.com>
2024-09-12 08:10:27 -0300

    Added transactions history list

---
+++ PocketMate.Web/Pages/Transactions/List.razor.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using PocketMate.Core.Common.Extensions;
+using PocketMate.Core.Handlers;
+using PocketMate.Core.Models;
+using PocketMate.Core.Requests.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketMate.Web.Pages.Transactions
+{
+    public class ListTransactionsPage : ComponentBase
+    {
+        #region properties
+        public bool IsBusy { get; set; } = false;
+
+        public List<Transaction> Transactions { get; set; } = [];
+
+        public string SearchTerm { get; set; } = "";
+
+        public int CurrentYear { get; set; } = DateTime.Now.Year;
+
+        public int CurrentMonth { get; set; } = DateTime.Now.Month;
+
+        public int[] Years { get; set; } =
+        { 
+            DateTime.Now.Year,
+            DateTime.Now.AddYears(-1).Year,
+            DateTime.Now.AddYears(-2).Year,
+            DateTime.Now.AddYears(-3).Year,
+            DateTime.Now.AddYears(-4).Year
+        };
+
+        #endregion
+
+        #region services
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public IDialogService DialogService { get; set; } = null!;
+
+        [Inject]
+        public ITransactionHandler TransactionHandler { get; set; } = null!;
+        #endregion
+
+        #region events
+        protected async override Task OnInitializedAsync()
+        {
+            await GetTransactions();
+        }
+        #endregion
+
+        #region methods
+        public Func<Transaction, bool> Filter => transaction =>
+        {
+            if (string.IsNullOrWhiteSpace(SearchTerm))
+                return true;
+
+            if (transaction.Id.ToString().Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            if (transaction.Title.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            return false;
+        };
+
+        private async Task GetTransactions()
+        {
+            IsBusy = true;
+
+            try
+            {
+                DateTime startDate = new DateTime(CurrentYear, CurrentMonth, 1);
+                var request = new GetTransactionsByPeriodRequest()
+                { 
+                    StartDate = DateTime.Now.GetFirstDay(CurrentYear, CurrentMonth),
+                    EndDate = DateTime.Now.GetLastDay(CurrentYear, CurrentMonth),
+                    PageNumber = 1,
+                    PageSize = 1000
+                };
+                var result = await TransactionHandler.GetByPeriodAsync(request);
+                if (result.IsSuccess)
+                {
+                    Transactions = result.Data ?? [];
+                }
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+            
+        }
+        #endregion
+    }
+}
